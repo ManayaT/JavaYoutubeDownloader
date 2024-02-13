97e63e302c259d629b5dfbc06ddb621e5e01198a
ManayaT <153574808+ManayaT@users.noreply.github.com>
2024-02-13 15:13:30 +0900

    Update InputURL.java

--- src/main/java/InputURL.java
+++ src/main/java/InputURL.java
@@ -4,7 +4,7 @@ public class InputURL {
     public String getInputURL() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.print("\n> VideoURL Example.\n \"https://www.youtube.com/watch?v=SrgASS4O8I8\"\n \"https://youtu.be/YP61xB66_Ic?si=XleALR3czQYnLpMj\"\nInputVideoURL >");
+            System.out.print("\n> VideoURL Example.\nInputVideoURL >");
             String inputURL = scanner.nextLine();
             // 対応していないURLの場合は再入力
             if (inputURL.contains("https://www.youtube.com/watch?v=") || inputURL.contains("https://youtu.be/")) {
