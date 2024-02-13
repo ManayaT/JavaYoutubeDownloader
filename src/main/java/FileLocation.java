cdc1e962168100353986c40e1233277cbfc196b7
ManayaT <153574808+ManayaT@users.noreply.github.com>
2024-02-13 15:14:12 +0900

    Update FileLocation.java

--- src/main/java/FileLocation.java
+++ src/main/java/FileLocation.java
@@ -8,7 +8,7 @@ public class FileLocation {
         File inputPath;
 
         while (true){
-            System.out.print("\n> File Location Example.\n  /Users/k22041kk/Downloads/YouTube\nFile Location? >");
+            System.out.print("\n> File Location Example.\n  /Users/k22000kk/Downloads\nFile Location? >");
             inputPath = new File(scanner.nextLine());
             // 保存先パスが存在しない場合は再入力
             if (inputPath.exists()) {
